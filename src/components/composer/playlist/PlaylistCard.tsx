879d1e37c4289f9eb6faf9fa59ff60366ec64956
moritz <theincxption@gmail.com>
2021-03-20 16:02:45 +0100

    :lipstick: Fix hover animation and adjust overlay color

--- src/components/composer/playlist/PlaylistCard.tsx
+++ src/components/composer/playlist/PlaylistCard.tsx
@@ -28,7 +28,7 @@ const PlaylistCard: React.FC<Props> = ({ playlist, isSelected, togglePlaylist })
 
     return (
         <motion.div
-            className={"bg-white shadow-md rounded-xl flex flex-col " +
+            className={"bg-white shadow-md rounded-xl flex flex-col group " +
             "justify-between cursor-pointer transition-all overflow-hidden " +
             (isSelected && "ring-4 ring-emerald-500")}
             variants={cardVariants}
@@ -38,7 +38,7 @@ const PlaylistCard: React.FC<Props> = ({ playlist, isSelected, togglePlaylist })
             onClick={() => togglePlaylist(playlist.id)}
         >
             <motion.div
-                className="absolute w-full h-full top-0 left-0 z-10 bg-black bg-opacity-30
+                className="absolute w-full h-full top-0 left-0 z-10 bg-black bg-opacity-10
                     flex justify-center items-center"
                 variants={overlayVariants}
             >
@@ -46,7 +46,7 @@ const PlaylistCard: React.FC<Props> = ({ playlist, isSelected, togglePlaylist })
                     xmlns="http://www.w3.org/2000/svg"
                     width="150"
                     height="150"
-                    style={{filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, .5))"}}
+                    style={{filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, .6))"}}
                 >
                     <motion.path
                         d="M38 74.707l24.647 24.646L116.5 45.5"
@@ -64,8 +64,8 @@ const PlaylistCard: React.FC<Props> = ({ playlist, isSelected, togglePlaylist })
             <div className="rounded-t-xl w-full h-44 shadow-sm transition-all overflow-hidden">
                 <div
                     style={{ backgroundImage: playlist.images[0] ? `url('${playlist.images[0].url}` : "linear-gradient(#4B5563, #1F2937)" }}
-                    className={"w-full h-full bg-cover bg-center transition-all transform hover:scale-110 " +
-                    (isSelected && "scale-110")}
+                    className={"w-full h-full bg-cover bg-center transition-all transform scale-110 group-hover:scale-100 " +
+                    (isSelected && "scale-100")}
                 />
             </div>
             <div className="w-full flex flex-row justify-between text-md tracking-tight px-3 py-2 flex-grow">
