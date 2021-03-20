c789c03e2d20abb5dd940841f0f64e21334b0e9e
moritz <theincxption@gmail.com>
2021-03-20 12:30:40 +0100

    :construction: Setup song picker component

--- src/components/composer/song/SongPicker.tsx
+++ src/components/composer/song/SongPicker.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { getAllSongs } from "../../../spotify/playlists"
+import { motion } from "framer-motion"
 
 interface Props {
     includedPlaylists: string[]
@@ -7,18 +8,57 @@ interface Props {
 
 const SongPicker: React.FC<Props> = ({ includedPlaylists }) => {
     const [songs, setSongs] = useState<any[]>([])
+    const [index, setIndex] = useState(0)
+    const [liked, setLiked] = useState<number[]>([])
 
     useEffect(() => {
-        console.log("includedPlaylists =", includedPlaylists)
         getAllSongs(includedPlaylists).then(songs => setSongs(songs))
     }, [includedPlaylists])
 
+    const next = () => setIndex(index + 1)
+    const like = () => {
+        setLiked(prevState => {
+            const newState = [...prevState]
+            newState.push(index)
+            return newState
+        })
+        next()
+    }
+
+    const currentSong = songs[index]
+    if (!currentSong) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
-            {songs.map(song => {
-                return <div key={song.track.id}>{song.track.name}</div>
-            })}
+            <div className="flex flex-col justify-center items-center mt-20">
+                <div style={{ backgroundImage: `url('${currentSong.track.album.images[0].url}` }}
+                     className="w-64 h-64 bg-cover mx-auto mb-5 bg-center shadow-sm"
+                />
+                <p className="text-lg font-semibold tracking-tight">
+                    {currentSong.track.name}
+                </p>
+                <p className="tracking-tight">
+                    {currentSong.track.artists.map((it: any) => it.name).join(", ")}
+                </p>
+                <div className="mt-10 flex flex-row justify-around">
+                    <motion.p
+                        className="mx-10 px-6 py-2 rounded-md bg-green-500 text-white cursor-pointer"
+                        whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}
+                        onClick={like}
+                    >
+                        Like
+                    </motion.p>
+                    <motion.p
+                        className="mx-10 px-6 py-2 rounded-md bg-red-500 text-white cursor-pointer"
+                        whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}
+                        onClick={next}
+                    >
+                        Dislike
+                    </motion.p>
+                </div>
+            </div>
         </div>
     )
 }
