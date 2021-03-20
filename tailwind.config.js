b896c7eba89eda62821594f45dc7f4d72b8d36bb
moritz <theincxption@gmail.com>
2021-03-20 16:03:23 +0100

    :wrench: Switch color palette and extend variants for "scale"

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: "class",
     theme: {
-        extend: {
-            colors: {
-                gray: colors.gray
-            }
+        colors: {
+            ...colors
         }
     },
     variants: {
-        extend: {}
+        extend: {
+            scale: ["group-hover"]
+        }
     },
     plugins: []
 }
