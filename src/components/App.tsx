70063dc8eaa53741cdd7264945b5f241229dd58f
moritz <theincxption@gmail.com>
2021-03-20 01:33:11 +0100

    :boom: Assemble core app logic in App.tsx component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,27 @@
 import React from "react"
+import Header from "./Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import AuthorizationCallback from "./authorization/AuthorizationCallback"
+import WithAuthorization from "./authorization/WithAuthorization"
+import Composer from "./composer/Composer"
 
 const App: React.FC = () => {
     return (
-        <div>
-
-        </div>
+        <Router>
+            <div className="w-full h-full bg-gradient-to-b from-gray-100 to-gray-200 select-none flex flex-col">
+                <Header/>
+                <Switch>
+                    <Route path="/authorization_callback">
+                        <AuthorizationCallback/>
+                    </Route>
+                    <Route path="/">
+                        <WithAuthorization>
+                            <Composer/>
+                        </WithAuthorization>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
