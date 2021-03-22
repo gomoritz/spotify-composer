d42807a91e6d2872fa7f039aa86a5dd7cfa963db
flaaaps <julianwening4@gmail.com>
2021-03-22 18:47:50 +0100

    :rocket: Include more music emojis

--- src/spotify/playlists.ts
+++ src/spotify/playlists.ts
@@ -3,31 +3,34 @@ import { getProfile } from "./profile"
 import { Playlist, PlaylistResponse, Song } from "../types/spotify"
 
 export async function getPlaylists(next?: string): Promise<Playlist[]> {
-    const response: PlaylistResponse = await fetch(
-        next ?? "https://api.spotify.com/v1/me/playlists?limit=50",
-        { headers: authorizationHeaders() }
-    ).then(res => res.json()).catch(console.error)
+    const response: PlaylistResponse = await fetch(next ?? "https://api.spotify.com/v1/me/playlists?limit=50", {
+        headers: authorizationHeaders(),
+    })
+        .then(res => res.json())
+        .catch(console.error)
 
     if (response.next) {
         console.log(`Got ${response.items.length}, fetching next...`)
-        return [...response.items, ...await getPlaylists(response.next)]
+        return [...response.items, ...(await getPlaylists(response.next))]
     }
 
     return response.items
 }
 
 export async function getPlaylist(id: string): Promise<Playlist> {
-    return await fetch(
-        "https://api.spotify.com/v1/playlists/" + id,
-        { headers: authorizationHeaders() }
-    ).then(res => res.json()).catch(console.error)
+    return await fetch("https://api.spotify.com/v1/playlists/" + id, {
+        headers: authorizationHeaders(),
+    })
+        .then(res => res.json())
+        .catch(console.error)
 }
 
 export async function getAllSongs(playlists: string[]): Promise<Song[]> {
     const result: Song[] = []
-    const isAlreadyAdded = (song: Song) => !!result.find(it =>
-        it.track.id === song.track.id || it.track.external_ids.isrc === song.track.external_ids.isrc
-    )
+    const isAlreadyAdded = (song: Song) =>
+        !!result.find(
+            it => it.track.id === song.track.id || it.track.external_ids.isrc === song.track.external_ids.isrc
+        )
 
     for (let id of playlists) {
         const playlist = await getPlaylist(id)
@@ -43,37 +46,34 @@ export async function createPlaylist(): Promise<Playlist> {
     const profile = await getProfile()
     if (!profile) throw new Error("User is not authenticated")
 
-    return await fetch(
-        `https://api.spotify.com/v1/users/${profile.id}/playlists`,
-        {
-            method: "POST",
-            headers: authorizationHeaders(),
-            body: JSON.stringify({
-                name: `${getRandomEmoji()} ${profile.display_name}'s Composed Playlist`,
-                description: "🔥 Generated with the Spotify Playlist Composer by Inception Cloud. " +
-                    "👉 Create your own one at composer.inceptioncloud.net!"
-            })
-        }
-    ).then(res => res.json()).catch(console.error)
+    return await fetch(`https://api.spotify.com/v1/users/${profile.id}/playlists`, {
+        method: "POST",
+        headers: authorizationHeaders(),
+        body: JSON.stringify({
+            name: `${getRandomEmoji()} ${profile.display_name}'s Composed Playlist`,
+            description:
+                "🔥 Generated with the Spotify Playlist Composer by Inception Cloud. " +
+                "👉 Create your own one at composer.inceptioncloud.net!",
+        }),
+    })
+        .then(res => res.json())
+        .catch(console.error)
 }
 
 export async function addSongsToPlaylist(playlistId: string, songs: Song[]): Promise<any> {
-    return await fetch(
-        "https://api.spotify.com/v1/playlists/" + playlistId + "/tracks",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                ...authorizationHeaders()
-            },
-            body: JSON.stringify({
-                uris: songs.map(song => song.track.uri)
-            })
-        }
-    )
+    return await fetch("https://api.spotify.com/v1/playlists/" + playlistId + "/tracks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            ...authorizationHeaders(),
+        },
+        body: JSON.stringify({
+            uris: songs.map(song => song.track.uri),
+        }),
+    })
 }
 
 function getRandomEmoji() {
-    const emojis = ["🔮", "🎵", "🎶", "🎙", "🎤", "🎧", "📯", "🎼", "🥁", "🎷", "🎺", "🎸", "🪕", "🎻", "🎹"]
+    const emojis = ["🔮", "🎵", "🎶", "🎙", "🎤", "🎧", "📯", "🎼", "🥁", "🎷", "🎺", "🎸", "🪕", "🎻", "🎹", "📻", "🎸"]
     return emojis[Math.floor(Math.random() * emojis.length)]
 }
