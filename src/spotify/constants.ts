3349ded165ac326c018a25862c8a89368cbf786e
moritz <theincxption@gmail.com>
2021-03-25 20:34:52 +0100

    :alien: Add "user-library-read" scope to access the user's library

--- src/spotify/constants.ts
+++ src/spotify/constants.ts
@@ -4,7 +4,7 @@ const constants = {
     redirect_uri: process.env.NODE_ENV === "development" ?
         "http://localhost:3000/authorization_callback" :
         "https://composer.inceptioncloud.net/authorization_callback",
-    scopes: encodeURIComponent("playlist-read-private playlist-modify-public playlist-read-collaborative")
+    scopes: encodeURIComponent("playlist-read-private playlist-modify-public playlist-read-collaborative user-library-read")
 }
 
 export default constants
