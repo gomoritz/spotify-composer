7f5d781fcf98b0686859fe492edcbcd03bd201c1
moritz <theincxption@gmail.com>
2021-04-07 14:46:40 +0200

    :wrench: Update redirect uri to new domain

--- src/spotify/constants.ts
+++ src/spotify/constants.ts
@@ -3,7 +3,7 @@ const constants = {
     response_type: "token",
     redirect_uri: process.env.NODE_ENV === "development" ?
         "http://localhost:3000/authorization_callback" :
-        "https://composer.inceptioncloud.net/authorization_callback",
+        "https://composer.incxption.dev/authorization_callback",
     scopes: encodeURIComponent("playlist-read-private playlist-modify-public playlist-read-collaborative user-library-read")
 }
 
