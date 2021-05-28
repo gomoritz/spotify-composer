21e047be988c7ccaad93e94df26b32cf77b44581
moritz <theincxption@gmail.com>
2021-05-28 11:45:16 +0200

    Restyle DialogButton and add "dangerous" flare

--- src/components/composer/song/DialogButton.tsx
+++ src/components/composer/song/DialogButton.tsx
@@ -4,17 +4,20 @@ import React from "react"
 type Props = {
     onClick(): void
     primary?: boolean
+    dangerous?: boolean
     className?: string
 }
 
-const DialogButton: React.FC<Props> = ({ onClick, primary, className, children }) => {
-    return <motion.div className={
-        `w-44 py-1 text-center rounded-md shadow-sm font-medium tracking-tight cursor-pointer 
-        ${primary ? "bg-emerald-300 text-emerald-800" : "bg-trueGray-200 text-trueGray-700"} 
-        ${className}`
-    }
-                       whileHover={{ scale: 1.02 }}
+const DialogButton: React.FC<Props> = ({ onClick, primary, dangerous, className, children }) => {
+    const classes = `w-44 py-1 text-center rounded-md shadow-sm font-medium tracking-tight cursor-pointer 
+        ${primary ? "bg-emerald-300 text-emerald-800" :
+            dangerous ? "bg-rose-200 text-rose-700" :
+                "bg-trueGray-200 text-trueGray-700"} ${className}`
+
+    return <motion.div className={classes}
+                       whileHover={{ scale: 1.02, filter: "brightness(96%)" }}
                        whileTap={{ scale: 0.95 }}
+                       animate={{ transition: { type: "spring" } }}
                        onClick={onClick}
     >
         {children}
